7408a113e7fe2944c67c42fe28037f7ffd3e62c7
= <=>
2020-11-02 23:41:15 +0700

    first commit - image

---
+++ app/src/main/java/com/DevayaniDinda/gd10_image_x_yyyy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.DevayaniDinda.gd10_image_x_yyyy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
