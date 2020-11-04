2e95129525e87c08d24412946c46d7f039fd5af0
= <=>
2020-11-04 22:28:34 +0700

    finish guided

---
+++ app/src/main/java/com/DevayaniDinda/gd10_image_x_yyyy/BitmapFragment.java
@@ -0,0 +1,19 @@
+package com.DevayaniDinda.gd10_image_x_yyyy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class BitmapFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bitmap, container, false);
+        return view;
+    }
+}
